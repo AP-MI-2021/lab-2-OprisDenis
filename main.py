a303788eaf0538dcbc9354b9c3c73e349d06e430
OprisDenis <92173022+OprisDenis@users.noreply.github.com>
2021-10-27 13:00:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 import math
 
 
-def is_prime(n):
-	if n == 2:
-		return True
-	if n < 2 or n%2 == 0:
-		return False
+def is_prime(n):#verifica daca un numar dat este prim
+    if n == 2:
+        return True
+    if n < 2 or n % 2 == 0:
+        return False
 
-	i=3
-	while i**2 <= n:
-		if n%i == 0:
-			return False
-		i += 2
-	return True
-
-
-def get_largest_prime_below(n):
-	if n <= 2:
-		return "(Error) n <= 2"
-
-	n -= 1
-	while not is_prime(n):
-		n -= 1
-
-	return n
+    i = 3
+    while i ** 2 <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
 
 
-def test_get_largest_prime_bellow():
-    assert get_largest_prime_bellow(14) == "13"
-    assert get_largest_prime_below(18) == "17"
-    assert get_largest_prime_below(24) == "23"
+def get_largest_prime_below(n):#cauta cel mai mare numar mai mic decat un numar dat si verifica daca acest numar este prim
+    if n <= 2:
+        return "(Error) n <= 2"
+
+    n -= 1
+    while not is_prime(n):
+        n -= 1
+
+    return n
 
 
-def  get_temp(temp,s_from,s_to):
+def test_get_largest_prime_below():
+    assert get_largest_prime_below(14) == 13
+    assert get_largest_prime_below(18) == 17
+    assert get_largest_prime_below(24) == 23
+
+
+def get_temp(temp, s_from, s_to): # Primeste ca parametri un float care reprezinta temperatura si doua caractere corespunzatoare unitatilor de masura pentru temperatura 
     temp = float(temp)
 
     if s_from not in ["C", "K", "F"]:
@@ -55,34 +55,40 @@ def  get_temp(temp,s_from,s_to):
         return to_f
     return to_k
 
+
 def test_get_temp():
-    assert get_temp(26.85,C,K) == "300"
-    assert get_temp(400,K,C) == "126.85"
+    assert get_temp(26.85, 'C', 'K') == "300"
+    assert get_temp(400, 'K', 'C') == "126.85"
 
 
 def get_cmmmc(nums):
     lcm = 1
     for i in nums:
-        lcm = lcm * i // math.gcd(lcm,i)
+        lcm = lcm * i // math.gcd(lcm, i)
     return lcm
 
 
+def tets_get_cmmmc():
+    assert get_cmmmc([1, 2, 3]) == 6
+    assert get_cmmmc([6,4,24]) == 24
+
+
 def run_get_cmmmc():
-	n = int(input("n: "))
+    n = int(input("n: "))
 
-	nums = []
+    nums = []
 
-	for i in range(n):
-		nums.append( int(input(f"nums[{i}]: ")) )
+    for i in range(n):
+        nums.append(int(input(f"nums[{i}]: ")))
 
-	print(f"cmmmc{nums} = {get_cmmmc(nums)}")
+    print(f"cmmmc{nums} = {get_cmmmc(nums)}")
 
 
 def menu():
-	print("0. quit()")
-	print("1. get_largest_prime_below(int n)")
-	print("13.get_temp(int temp, string from, string to)" )
-	print("14.get_cmmmc(list numbers)" )
+    print("0. quit()")
+    print("1. get_largest_prime_below(int n)")
+    print("13.get_temp(int temp, string from, string to)")
+    print("14.get_cmmmc(list numbers)")
 
 
 def main():
@@ -93,13 +99,13 @@ def main():
     while choice:
         choice = int(input("choice: "))
         if choice == 1:
-            n=int(input("get largest prime number below: "))
+            n = int(input("get largest prime number below: "))
             print(get_largest_prime_below(n))
         if choice == 13:
-            temp=float(input("write the tempreature you would like to convert "))
-            init_unit=input()
-            to_unit=input()
-            print(get_temp(temp, init_unit, to_unit ))
+            temp = float(input("write the tempreature you would like to convert "))
+            init_unit = input()
+            to_unit = input()
+            print(get_temp(temp, init_unit, to_unit))
         if choice == 14:
             print(run_get_cmmmc())
 
